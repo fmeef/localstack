93e572d612804799e9d48bf87733a40d0cd1a635
Alex Ballmer <=>
2020-09-26 15:23:20 -0500

    setup temporary directory

--- stack/localstack.go
+++ stack/localstack.go
@@ -1,9 +1,14 @@
 package stack
 
 import (
+	"context"
 	"fmt"
+	"path"
+	"os"
+	"github.com/docker/docker/client"
 	"github.io/gnu3ra/localstack/buildtemplates"
 	"github.io/gnu3ra/localstack/utils"
+	"github.com/jhoonb/archivex"
 )
 
 
@@ -29,9 +34,12 @@ type DockerStackConfig struct {
 
 
 type DockerStack struct {
-	Config *DockerStackConfig
+	config *DockerStackConfig
 	renderedBuildScript []byte
-	BuildScriptFileLocation string
+	buildScriptFileLocation string
+	dockerClient *client.Client
+	ctx context.Context
+	statePath string
 }
 
 
@@ -42,10 +50,84 @@ func NewDockerStack(config *DockerStackConfig) (*DockerStack, error) {
 		return nil, fmt.Errorf("Failed to render build script %v", err)
 	}
 
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create docker api client: %v", err)
+	}
+
 	stack := &DockerStack{
-		Config:	config,
+		config:	config,
 		renderedBuildScript: renderedBuildScript,
+		ctx: ctx,
+		dockerClient: cli,
+		statePath: path.Join(path.Clean(config.StatePath), ".localstack"),
 	}
 
 	return stack, nil
+}
+
+
+
+func (s *DockerStack) setupTmpDir() error {
+	tar := new(archivex.TarFile)
+
+	os.MkdirAll(path.Join(s.statePath, "build-ubuntu"), os.ModeDir)
+	os.MkdirAll(path.Join(s.statePath, "mounts/script"), os.ModeDir)
+	os.MkdirAll(path.Join(s.statePath, "mounts/keys"), os.ModeDir)
+	os.MkdirAll(path.Join(s.statePath, "mounts/logs"), os.ModeDir)
+	os.MkdirAll(path.Join(s.statePath, "mounts/release"), os.ModeDir)
+
+	dockerFile, err := utils.RenderTemplate(buildtemplates.DockerTemplate, s.config)
+
+	if err != nil {
+		return fmt.Errorf("failed to render docker template: %v", err)
+	}
+
+	ibd, err := os.Create(path.Join(s.statePath, "build-ubuntu/install-build-deps.sh"))
+
+	if err != nil {
+		return fmt.Errorf("failed to create install-build-deps.sh: %v", err)
+	}
+
+	defer ibd.Close()
+
+	ibd.WriteString(buildtemplates.ChromiumDeps)
+	ibd.Sync()
+
+	iad, err := os.Create(path.Join(s.statePath, "build-ubuntu/install-build-deps-android.sh"))
+
+	if err != nil {
+		return fmt.Errorf("failed to create install-build-deps-android.sh: %v", err)
+	}
+
+	defer iad.Close()
+
+	iad.WriteString(buildtemplates.AndroidDeps)
+	iad.Sync()
+
+	df, err := os.Create(path.Join(s.statePath, "build-ubuntu/Dockerfile"))
+
+	if err != nil {
+		return fmt.Errorf("failed to write dockerfile")
+	}
+	df.Write(dockerFile)
+	df.Sync()
+
+	defer df.Close()
+
+	bs, err := os.Create(path.Join(s.statePath, "mounts/script/build.sh"))
+
+	if err != nil {
+		return fmt.Errorf("failed to write build script")
+	}
+
+	bs.Write(s.renderedBuildScript)
+	bs.Sync()
+
+	tar.Create(path.Join(s.statePath, "build-ubuntu.tar"))
+	tar.AddAll(path.Join(s.statePath, "build-ubuntu"), true)
+	tar.Close()
+	return nil
 }
